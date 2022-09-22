0fb4d2e5d45401de27cb550dc747374fbd4e22d9
Yurkes2020 <yurkes2020@gmail.com>
2022-09-22 15:11:44 +0300

    fetch

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,23 +1,22 @@
-const API_KEY = '29907105-27da4e6e42fdff29794422632';
-const BASE_URL = 'https://pixabay.com/api/';
 export default class ApiService {
   constructor() {
     this.searchQuery = '';
   }
 
-  fetchPhotos() {
+  fetchImages() {
     console.log(this);
-    const url = `&{BASE_URL}?&{API_KEY}&q=&{this.searchQuery}&page=${this.page}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`;
-    return fetch(url)
+    const url = `https://pixabay.com/api/?key=29907105-27da4e6e42fdff29794422632&q=${this.searchQuery}&image_type=photo&page=1&per_page=40&orientation=horizontal&safesearch=true`;
+
+    fetch(url)
       .then(response => response.json())
-      .then(console.log(data));
+      .then(console.log);
   }
 
   get query() {
     return this.searchQuery;
   }
 
-  set query(newQuery) {
-    return (this.searchQuery = newQuery);
+  set(newQuery) {
+    this.searchQuery = newQuery;
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -4,15 +4,52 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import ApiService from './fetch';
 
-const searchQuery = document.querySelector('.search-form');
-console.log(searchQuery);
-
-searchQuery.addEventListener('submit', onSearch);
-
 const apiService = new ApiService();
+
+const searchForm = document.querySelector('.search-form');
+const galleryRef = document.querySelector('.gallery');
+const loadMore = document.querySelector('.load-more');
+
+searchForm.addEventListener('submit', onSearch);
+loadMore.addEventListener('click', onLoadMore);
 
 function onSearch(e) {
   e.preventDefault();
   apiService.query = e.currentTarget.elements.searchQuery.value;
-  apiService.fetchPhotos();
+  apiService.fetchImages();
 }
+
+function onLoadMore() {
+  apiService.fetchImages();
+}
+
+const createGalleryItem = ({ preview, original, description }) => {
+  return `
+    <div class="photo-card">
+  <a class="gallery__item" href=""><img src="" alt="" loading="lazy" /></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>`;
+};
+
+const createGalleryMarkup = item => {
+  const galleryMarkup = item.map(createGalleryItem).join('');
+  galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
